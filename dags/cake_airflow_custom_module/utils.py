47ec55e5c74f7e0f51dc1cbd0afdbb9e24062847
vandang <dangtrivan15@gmail.com>
2024-05-03 21:43:32 +0700

    feat: init test dag with custom module embedded

---
+++ dags/cake_airflow_custom_module/utils.py
@@ -0,0 +1,2 @@
+def test():
+    print("OK Test Passed!")
---
+++ dags/my_dag.py
@@ -0,0 +1,14 @@
+from cake_airflow_custom_module.utils import test
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+with DAG(
+        dag_id="test_dag",
+        start_date=datetime(2021, 1, 1),
+        schedule=None
+):
+    PythonOperator(
+        task_id="test_task",
+        python_callable=test,
+    )
